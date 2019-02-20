29b130fc55d254dcb14ca14cbca4746484f745bc
yangdan01 <yangdan01@dtinsure.com>
2019-02-20 15:14:43 +0800

    first commit

---
+++ src/components/introduce.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item,index) in list" :key="index">{{item.name}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    props:['list']
+}
+</script>
+
---
+++ src/pages/index/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+
+    <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <div class="userinfo-nickname">
+        <card :text="userInfo.nickName"></card>
+      </div>
+    </div>
+
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+    <div class="userList">
+      <div class="cl">请选择你喜欢的明星</div>
+      <introduce :list="starsList"></introduce>
+    </div>
+
+    <form class="form-container">
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+import introduce from '@/components/introduce'
+export default {
+  data () {
+    return {
+      motto: 'This is my first mpvue project',
+      userInfo: {},
+      starsList: [{
+        name: '靳东',
+        id: '1'
+      },{
+        name: '胡歌',
+        id: '2'
+      },{
+        name: '吴亦凡',
+        id: '2'
+      }]
+    }
+  },
+
+  components: {
+    card,
+    introduce
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+
+.counter {
+  display: inline-block;
+  margin: 10px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
